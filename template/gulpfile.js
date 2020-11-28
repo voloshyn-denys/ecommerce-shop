bc8a7ecbbbf2e651d7d89ffb10534d5baadf8ff6
Denys Voloshyn <voloshyn.denys@gmail.com>
2020-11-28 21:21:14 +0200

    add template

---
+++ template/gulpfile.js
@@ -0,0 +1,17 @@
+const { watch, src, dest } = require('gulp');
+const sass = require('gulp-sass');
+const cssnano = require('gulp-cssnano');
+
+const styles = () => {
+    return src('./sass/**/*.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cssnano())
+        .pipe(dest('./css'))
+};
+
+const watcher = () => {
+    watch('./sass/**/*.sass', styles);
+}
+
+exports.sass = styles;
+exports.default = watcher;
